6edfc2f5abe29a48a56dcba47e4340ec5d863ffd
Naveen <122507683+Naveenpoliasetty@users.noreply.github.com>
2024-05-10 12:44:17 +0530

    Update X1_process_data.py

--- X1_process_data.py
+++ X1_process_data.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 import sklearn
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('mushrooms.csv')
 data = pd.get_dummies(df.drop(columns=['class']), df.columns[1:]).join(df['class'] == 'e')
